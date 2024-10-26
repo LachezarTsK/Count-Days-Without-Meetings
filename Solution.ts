ce737ed6e5458d5734651835c13134137e1503cc
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-10-26 12:31:16 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,24 @@
+
+function countDays(numberOfDays: number, meetings: number[][]): number {
+
+    const FIRST_DAY = 1;
+    meetings.sort((x, y) => x[0] - y[0]);
+    let totalMinStartTime = meetings[0][0];
+    let totalMaxEndTime = Number.MIN_SAFE_INTEGER;
+
+    for (let meeting of meetings) {
+        totalMaxEndTime = Math.max(totalMaxEndTime, meeting[1]);
+    }
+
+    let latestMaxEndTime = meetings[0][1];
+    let daysAvailableForWorkWithoutMeetings = (totalMinStartTime - FIRST_DAY) + (numberOfDays - totalMaxEndTime);
+
+    for (let i = 1; i < meetings.length; ++i) {
+        if (latestMaxEndTime < meetings[i][0]) {
+            daysAvailableForWorkWithoutMeetings += meetings[i][0] - latestMaxEndTime - 1;
+        }
+        latestMaxEndTime = Math.max(latestMaxEndTime, meetings[i][1]);
+    }
+
+    return daysAvailableForWorkWithoutMeetings;
+};
